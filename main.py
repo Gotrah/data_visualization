ac9a04bc991d35b8c47326f71ca735bb24c98ede
Alex Hartog <hartog.alex@gmail.com>
2023-04-06 17:40:13 +0200

    Cubes with colormap

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 
-input_values = []
-squares = []
+# input_values = []
+# squares = []
+#
+# for i in range(1, 10):
+#     input_values.append(i)
+#     squares.append(i * i)
 
-for i in range(1, 10):
-    input_values.append(i)
-    squares.append(i * i)
+input_values = range(1, 1001)
+cubes = [x ** 3 for x in input_values]
 
 plt.style.use('seaborn')
 # input_values = [1, 2, 3, 4, 5]
@@ -13,7 +16,10 @@ plt.style.use('seaborn')
 
 fig, ax = plt.subplots()
 # ax.plot(input_values, squares, linewidth=3)
-ax.scatter(input_values, squares, s=100)
+ax.scatter(input_values, cubes, c=cubes, cmap=plt.cm.Reds, s=10)
+
+# ax.axis([0, 1100, 0, 1_100_000])
+# ax.ticklabel_format(style='plain')
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -24,3 +30,4 @@ ax.set_ylabel("Square of Value", fontsize=14)
 ax.tick_params(labelsize=14)
 
 plt.show()
+# plt.savefig('squares_plot.png', bbox_inches='tight')
