2f6966279d76b296ce45e2358f7aff595130855c
Alex <hartog.alex@gmail.com>
2023-04-22 17:06:54 +0200

    Final version of earth quake map

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import plotly.express as px
 
+
 def save_formatted_data(data):
     # Write formatted earthquake data to file
     formatted_path = Path('eq_data/readable_eq_data.geojson')
@@ -12,7 +13,7 @@ def save_formatted_data(data):
 
 # Read earthquake data from file
 file_path = Path('eq_data/eq_data_30_day_m1.geojson')
-with file_path.open() as file:
+with file_path.open(encoding="utf8") as file:
     eq_data_json = file.read()
 
 # Convert JSON data to Python object
@@ -22,25 +23,18 @@ all_eq_data = json.loads(eq_data_json)
 all_eq_dicts = all_eq_data['features']
 
 mags, lons, lats = [], [], []
-eq_title = [eq['properties']['title'] for eq in all_eq_dicts]
-for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['mag']
-    lon = eq_dict['geometry']['coordinates'][0]
-    lat = eq_dict['geometry']['coordinates'][1]
-    mags.append(mag)
-    lons.append(lon)
-    lats.append(lat)
+mags = [eq_dict['properties']['mag'] for eq_dict in all_eq_dicts]
+lons = [eq_dict['geometry']['coordinates'][0] for eq_dict in all_eq_dicts]
+lats = [eq_dict['geometry']['coordinates'][1] for eq_dict in all_eq_dicts]
+eq_titles = [eq['properties']['title'] for eq in all_eq_dicts]
 
-# print(mags[:10])
-# print(lons[:5])
-# print(lats[:5])
-
-title = 'Global Earthquakes'
+title = all_eq_data['metadata']['title']
 fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title,
                      color=mags,
                      color_continuous_scale='Viridis',
                      labels={'color':'Magnitude'},
                      projection='natural earth',
+                     hover_name=eq_titles,
                      )
 fig.show()
 
